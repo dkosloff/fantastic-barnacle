fc758ec1815344a2052e4bd89df97d629c403424
Dylan <dylankosloff@gmail.com>
2018-08-21 23:25:15 -0700

    Adding HammingDistance

---
+++ BioInformatics I/HammingDistance.py
@@ -0,0 +1,15 @@
+def HammingDistance( stringA, stringB):
+    distance = 0
+    for index, letter in enumerate(stringA):
+        if stringB[index] != letter or index >= len(stringB):
+            distance += 1
+
+    if(len(stringB) > len(stringA)):
+        distance += len(stringB) - len(stringA)
+    return distance
+
+with open('../../data/dataset_9_3.txt') as inputFile:
+    compareFirst = inputFile.readline().rstrip()
+    compareSecond = inputFile.readline().rstrip()
+    
+print(HammingDistance(compareFirst, compareSecond))
