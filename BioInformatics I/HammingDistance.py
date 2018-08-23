0feea1a9206d8a6154b8c09e92598b402c7ddf78
Dylan K <dylankosloff@gmail.com>
2018-08-22 18:26:30 -0700

    Adding pydoc notation to HammingDistance

--- BioInformatics I/HammingDistance.py
+++ BioInformatics I/HammingDistance.py
@@ -1,4 +1,15 @@
 def HammingDistance(stringA, stringB):
+    '''Finds the number of characters that vary
+    between two strings
+    
+    Arguments:
+        stringA {string} -- The first string to compare
+        stringB {string} -- The second string to compare
+    
+    Returns:
+        integer -- The number of characters that are different
+    '''
+
     distance = 0
     for index, letter in enumerate(stringA):
         if stringB[index] != letter or index >= len(stringB):
@@ -8,8 +19,8 @@ def HammingDistance(stringA, stringB):
         distance += len(stringB) - len(stringA)
     return distance
 
-with open('../../data/dataset_9_3.txt') as inputFile:
-    compareFirst = inputFile.readline().rstrip()
-    compareSecond = inputFile.readline().rstrip()
+# with open('../../data/dataset_9_3.txt') as inputFile:
+#     compareFirst = inputFile.readline().rstrip()
+#     compareSecond = inputFile.readline().rstrip()
     
-print(HammingDistance(compareFirst, compareSecond))
+# print(HammingDistance(compareFirst, compareSecond))
