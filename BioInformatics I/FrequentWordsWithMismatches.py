38d6a07f35afeb54da45b93c9f1ea2c57406e701
Dylan K <dylankosloff@gmail.com>
2018-08-22 18:24:52 -0700

    Add files via upload

---
+++ BioInformatics I/FrequentWordsWithMismatches.py
@@ -0,0 +1,28 @@
+def MostFrequentWithMismatches( test, k, d):
+    '''Outputs a file with the most frequent words of k-length 
+    that almost match the text (the matches can be off by d characters)
+
+    Arguments:
+        test {str} -- The string to be searched
+        k {int} -- The length of the words to search for
+        d {int} -- The number of characters that can be wrong and
+        still consider the word a match
+    '''
+
+
+def Neighbors(pattern, d):
+    '''Returns all the neighboring nucleotide combinations for a
+    given pattern and prefix count (?)
+    
+    Arguments:
+        pattern {[type]} -- [description]
+        d {[type]} -- [description]
+    
+    Returns:
+        [type] -- [description]
+    '''
+
+    neighborhood = []
+
+
+    return neighborhood
