52071de30d0af9bb9986f4d646e435e99140adc8
Kosloff, Dylan <dylan.kosloff@progeny.net>
2018-09-14 15:43:53 -0700

    Added class to contain code for BioInfo2

---
+++ BioInformatics II/GenomeSequencer.py
@@ -0,0 +1,8 @@
+class Sequencer:
+
+    def __init__(self):
+        pass
+        # Any initialization, class data members you need
+        # would be put here
+    
+    #  There are no private members or methods
